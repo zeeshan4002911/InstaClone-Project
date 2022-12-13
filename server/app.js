e8d7444e305d5e6dfd4eb3a284a65e19c40c1be9
AAA4002911 <zeeshan4002911@gmail.com>
2022-12-13 14:28:13 +0530

    cors fix

--- server/app.js
+++ server/app.js
@@ -28,6 +28,13 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage })
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.get("/", (req, res) => {
     res.json({
         status: "Running",
@@ -42,7 +49,7 @@ app.get("/post", async (req, res) => {
             status: "Error",
             message: err.message
         })
-        res.json({data: response});
+        res.json({ data: response });
     })
 })
 
