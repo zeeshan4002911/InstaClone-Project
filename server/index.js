1d04badc578c976a2112f99e1215906bc886071d
AAA4002911 <zeeshan4002911@gmail.com>
2022-12-13 13:09:43 +0530

    warning bug fix

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 const app = require("./app");
+mongoose.set('strictQuery', true);
 
 const server = process.env.DATABASE_URL || "mongodb://localhost:27017/post";
 
