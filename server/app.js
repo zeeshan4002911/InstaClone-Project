4fbe53825f5304f30b236f06b6f17269c5879d9f
AAA4002911 <zeeshan4002911@gmail.com>
2022-12-13 15:01:34 +0530

    fix headers

--- server/app.js
+++ server/app.js
@@ -31,7 +31,7 @@ const upload = multer({ storage: storage })
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "*");
     next();
 });
 
